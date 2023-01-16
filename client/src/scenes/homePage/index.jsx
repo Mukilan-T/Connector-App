af4b46571467965c675903a1491215583d8300aa
Sabarish <studysabarish@gmail.com>
2023-01-16 13:38:58 +0530

    Added user profile page

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -5,6 +5,8 @@ import Navbar from "../navbar/index.jsx";
 import UserWidget from "scenes/widgets/UserWidgets.jsx";
 import MyPostWidgets from "scenes/widgets/MyPostWidgets.jsx";
 import PostsWidget from "scenes/widgets/PostsWidget.jsx";
+import AdvertWidget from "scenes/widgets/AdvertWidget.jsx";
+import FriendListWidget from "scenes/widgets/FriendListWidget.jsx";
 
 const HomePage = () => {
 
@@ -29,7 +31,9 @@ const HomePage = () => {
                 </Box>
                 {isNonMobileScreens && (
                     <Box flexBasis="26%">
-                        
+                        <AdvertWidget />
+                        <Box m="2rem 0" />
+                        <FriendListWidget userId={_id} />
                     </Box>
                 )}
             </Box>
